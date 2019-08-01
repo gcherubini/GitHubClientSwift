e8f89a0000e196c1d21ab55d0496a00592f99d6f
Guilherme Cherubini <gabreu@thoughtworks.com>
2019-07-31 21:51:03 -0300

    Add cocoapods and rxswift to the project

---
+++ Pods/Target Support Files/Pods-GitHubClientSwift/Pods-GitHubClientSwift-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GitHubClientSwift : NSObject
+@end
+@implementation PodsDummy_Pods_GitHubClientSwift
+@end
---
+++ Pods/Target Support Files/Pods-GitHubClientSwiftTests/Pods-GitHubClientSwiftTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GitHubClientSwiftTests : NSObject
+@end
+@implementation PodsDummy_Pods_GitHubClientSwiftTests
+@end
