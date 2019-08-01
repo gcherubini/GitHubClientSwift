8e84f681300017a80775d7a9b39b5a7b7887b485
Guilherme Cherubini <gabreu@thoughtworks.com>
2019-07-31 21:44:26 -0300

    Initial project

---
+++ GitHubClientSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubClientSwift
+//
+//  Created by Guilherme Cherubini on 31/07/19.
+//  Copyright © 2019 Cherubini Sperotto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
